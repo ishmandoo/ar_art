afb0f915902e9d1d4ac199404a548d298cb3cdc4
Ben <ishmandoo@gmail.com>
2017-03-12 16:59:12 -0400

    initial

---
+++ serve.py
@@ -0,0 +1,6 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()
